e2e5353ec61ce00861a55575b35c5c56162b5c59
luan moraes <luan.moraees21@gmail.com>
2023-11-27 21:21:32 -0300

    configurando knex e criando migration inicial

--- src/config/database.js
+++ src/config/database.js
@@ -1,1 +1,14 @@
-import knex form 'knex'
+import Knex from 'knex'
+
+const knexConfig = {
+  client: 'mysql2',
+  connection: {
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: 3306,
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '010203',
+    database: process.env.DB_NAME || 'financas'
+  }
+};
+
+export default knexConfig;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import knex from './config/database.js'
 dotenv.config();
 import useRoute from './modules/user/user.route.js'
 const app = express();
