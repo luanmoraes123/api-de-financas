8d5147215b50593a22f1b872fc2b8f77e54dbc58
luan moraes <luan.moraees21@gmail.com>
2023-11-28 18:49:17 -0300

    login adicionado

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -1,2 +1,23 @@
-export const login = (params) => { };
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import { save, getByEmail } from '../user/user.model.js';
+
+
+
+export const login = async (params) => {
+  const user = await getByEmail(params.email);
+  if (!user) {
+    return { error: "invalid password or email" }
+  }
+
+  const password = bcrypt.compareSync(params.password, user.password);
+
+  if (!password) {
+    return { error: "invalid password or email" }
+  }
+
+  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+  return { token }
+};
+
 export const register = (params) => { };
--- src/server.js
+++ src/server.js
@@ -2,12 +2,14 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import useRoute from './modules/user/user.route.js'
+import authRouter from './modules/auth/auth.route.js'
 const app = express();
 
 
 app.use(express.json());
 
 app.use('/user', useRoute);
+app.use('/auth', authRouter);
 
 
 app.listen(8080, () => {
