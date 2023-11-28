7043605dbd8c4674be3344c5f8a411cb29c0a98c
luan moraes <luan.moraees21@gmail.com>
2023-11-28 20:54:02 -0300

    regra de registro

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -20,4 +20,16 @@ export const login = async (params) => {
   return { token }
 };
 
-export const register = (params) => { };
+export const register = async (params) => {
+  const user = await getByEmail(params.email);
+  console.log(user);
+  if (user) {
+    return { error: "user alredy exists" }
+  }
+
+  const userCreated = await save(params);
+
+  const token = jwt.sign({ id: userCreated[0] }, process.env.JWT_SECRET);
+  return { token }
+
+};
